43c0fb4ea578bcf909ba0ed3b12a56bbbd72954d
Abhishek312003 <99153654se@gmail.com>
2025-02-14 00:10:11 +0530

    new code

--- python.py
+++ python.py
@@ -131,10 +131,115 @@ s = {8,7,12,"Abhishek",[1,2]}
 s[4][0] = 9
 # it will give a error we cannot include list in a set and list is unhashable
 
-    
+# CONDITIONAL EXPRESSIONS
+# write a program to print yes hwn the age entered by the user is greater than or equal to 18
+age = int(input("enter the age :"))
+
+if age >= 18:
+    print("yes")
+else:
+    print("No") 
+print("End of Program")
+
+# write a program to find the greatest of four numbers entered ny user
+a1 = int(input("enter the number 1:"))
+a2 = int(input("enter the number 2:"))
+a3 = int(input("enter the number 3:"))
+a4 = int(input("enter the number 4:"))
+
+if (a1>a2 and a1>a3 and a1>a4):
+   print("greatest number is a1", a1)
 
 
+elif (a2>a1 and a2>a3 and a2>a4):
+   print("greatest number is a2", a2)
 
 
+elif (a3>a1 and a3>a2 and a3>a4):
+   print("greatest number is a3", a3)
 
 
+elif (a4>a1 and a4>a2 and a4>a3):
+   print("greatest number is a4", a4)
+
+# write a program to find out whether a student has passed or failed if it required a total of  40%
+# and at least 33% in each subject to pass.Assume 3 subjects and take marks as an input from the user
+marks1 = int(input("Enter Marks 1: "))
+marks2 = int(input("Enter Marks 2: "))
+marks3 = int(input("Enter Marks 3: "))
+
+# Check for total percentage
+total_percentage = (100*(marks1 + marks2 + marks3))/300
+
+if(total_percentage>=40 and marks1>=33 and marks2>=33 and marks3>=33):
+    print("You are passed:", total_percentage)
+
+else:
+    print("You failed, try again next year:", total_percentage)
+
+#.a spam comment is defined as a text containing following keywords:
+# "make alot of money","buy now", "subscribe this","click this".write a program
+# to detect these spams
+p1 = "Make a lot of money"
+p2 = "buy now"  
+p3 = "subscribe this"  
+p4 = "click this"
+
+message = input("Enter your comment: ")
+
+if((p1 in message) or (p2 in message )or (p3 in message) or (p4 in message)):
+    print("This comment is a spam")
+
+else:
+    print("This comment is not a spam")
+ 
+# write a program to find whether a given username contains less than 10 characters or not?
+username = input("Enter username: ")
+
+if(len(username)<10):
+    print("Your username contains less than 10 characters")
+else:
+    print("Your username contains more than or equal to 10 characters")
+
+# write a program to finds out whether a given name is present in a list or not
+l = ["Abhishek", "Rohan", "Shubham", "Divya"]
+
+name = input("Enter your name: ")
+
+if(name in l):
+    print("Your name is in the list")
+else:
+    print("Your name is not in the list")
+
+# write a program to calculate the grade of a student from his marks from the following scheme:
+# 90 - 100 => Ex
+# 80 - 90 => A
+# 70 - 80 => B
+# 60 - 70 => C
+# 50 - 60 => D
+# <50 => F
+marks = int(input("Enter your marks: "))
+
+if(marks<=100 and marks>=90):
+    grade = "Ex"
+elif(marks<90 and marks>=80):
+    grade = "A"
+elif(marks<80 and marks>=70):
+    grade = "B"
+elif(marks<70 and marks>=60):
+    grade = "C"
+elif(marks<60 and marks>=50):
+    grade = "D"
+elif(marks<50):
+    grade = "F"
+
+print("Your grade is:", grade)
+
+# write a program to find out whether a given post is talking about "Abhishek" or not?
+post = input("Enter the post: ")
+
+if("Abhishek" in post.lower()):
+    print("This post is talking about Abhishek")
+
+else:
+    print("This post is not talking about Abhishek")
