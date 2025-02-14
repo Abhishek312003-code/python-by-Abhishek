d4b7065fdea5c6f8b69de52bab16467f00e453da
Abhishek312003 <99153654se@gmail.com>
2025-02-14 23:46:00 +0530

    new code

--- python.py
+++ python.py
@@ -131,6 +131,7 @@ s = {8,7,12,"Abhishek",[1,2]}
 s[4][0] = 9
 # it will give a error we cannot include list in a set and list is unhashable
 
+
 # CONDITIONAL EXPRESSIONS
 # write a program to print yes hwn the age entered by the user is greater than or equal to 18
 age = int(input("enter the age :"))
@@ -241,5 +242,102 @@ post = input("Enter the post: ")
 if("Abhishek" in post.lower()):
     print("This post is talking about Abhishek")
 
+
+# loops in python
+l = ["Abhishek" ,False,"help","case","popular","junior"]
+i = 0
+while (i<len(l)):
+    print(l[i])
+    i += 1
+
+# write a program to print multiplication of a given number using loop
+n = int(input("enter the number :"))
+for i in range (1,11):
+    print(f" {n} * {i} = {n*i}")
+
+# write a program to greet all the person names stored in a list "l" and which starts with S
+    # l = ["Abhishek" , "soham" , "Sachin" , "Rahul"]
+l = ["Abhishek" , "Soham" , "Sachin" , "Rahul"]
+for name in l:
+    if(name.startswith("S")):
+        print(f"Hello {name}")
+
+# write a program of problem no. 1 using while loop
+n = int(input("enter the number :"))
+i = 1
+while (i < 11):
+    print(f" {n} * {i} = {n*i}")
+    i += 1
+
+# write a program to find whether a givem number is prime or not
+n = int(input("Enter a number: "))
+
+for i in range(2, n):
+    if(n%i) == 0:
+        print("Number is not prime")
+        break
 else:
-    print("This post is not talking about Abhishek")
+    print("Number is prime")
+
+# write the program to find the sum of first n natural numbers using while loop
+n = int(input("Enter the number: "))
+i = 1
+sum = 0
+while(i<=n):
+    sum += i
+    i+=1
+
+print(sum)
+
+# write a program to find the factorial of a given number using for loop
+# 5! = 1 X 2 X 3 X 4 X 5
+n = int(input("Enter the number: "))
+product = 1
+for i in range(1, n+1):
+    product = product * i
+
+print(f"The factorial of {n} is {product}")
+
+# write the program to print the following star pattern
+# *
+# ***
+# ***** for n = 3
+n = int(input("Enter the number: "))
+for i in range(1, n+1):
+    print(" "* (n-i), end="")
+    print("*"* (2*i-1), end="")
+    print("")
+
+#write the program to print following star pattern
+# *
+# **
+# *** for n = 3
+n = int(input("Enter the number: "))
+for i in range(1, n+1): 
+    print("*"* i, end="")
+    print("")
+
+# write the program to print following star pattern
+'''
+
+***
+* *       for n = 3
+***
+
+
+'''
+n = int(input("Enter the number: "))
+for i in range(1, n+1): 
+    if(i==1 or i==n):
+        print("*"* n, end="")
+    else:
+        print("*", end="")
+        print(" "* (n-2), end="")
+        print("*", end="")
+    print("")
+
+# write a program to print multiplication table of n using for loops in reversed
+n = int(input("Enter the number: "))
+
+for i in range(1, 11):
+    print(f"{n} X {11 -i} = {n*(11-i)}")
